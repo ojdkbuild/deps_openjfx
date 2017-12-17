5c9d4ef8da7c3806198bf837478aa9d43424ed25
akashche <akashche@redhat.com>
2017-12-17 07:01:21 -0800

    enable web module

--- resources/run-gradle.bat
+++ resources/run-gradle.bat
@@ -16,13 +16,8 @@ rem limitations under the License.
 
 @echo on
 
-rem ojfx env
-set PATH=%PATH%;${OJDKBUILD_DIR}/tools/ant182/bin
-set PATH=%PATH%;${OJDKBUILD_DIR}/tools/gradle18/bin
-set PATH=%PATH%;${OJDKBUILD_DIR}/tools/cygwin64/bin
-set DXSDK_DIR=${OJDKBUILD_DIR}/tools/toolchain/directx-june2010
-set BASECLASSES_SDK_DIR=%WINDOWSSDKDIR%
-set MSVCR100_DIR=${OJDKBUILD_DIR}/tools/toolchain/msvcr100/${openjdk_MSVCR_PATH}
+rem env setup
+call "${OJDKBUILD_DIR}/resources/scripts/set-compile-env-vs15-x86_64.bat"
 
 rem freshly-built jdk
 set JAVA_HOME=${${PROJECT_NAME}_OJFX_JAVA_HOME}
@@ -33,6 +28,5 @@ if exist ".gradle" exit /b 1
 rd /s /q "buildSrc/.gradle"
 if exist "buildSrc/.gradle" exit /b 1
 cmd /c gradle cleanAll --refresh-dependencies --recompile-scripts --info || exit /b 1
-cmd /c gradle assemble --refresh-dependencies --info || exit /b 1
-cmd /c gradle test -x :web:test -x :fxpackager:test --refresh-dependencies --info || exit /b 1
+rem cmd /c gradle test -x :web:test -x :fxpackager:test --refresh-dependencies --info || exit /b 1
 cmd /c gradle zips -x :apps --refresh-dependencies --info || exit /b 1
